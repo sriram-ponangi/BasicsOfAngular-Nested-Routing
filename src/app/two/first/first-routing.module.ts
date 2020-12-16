b31f4f2c458e8ec69bc4b4fb73156fa4576c8799
Sriram Ponangi <sr810178@dal.ca>
2020-12-16 18:34:54 -0400

    adding routing code and html for two/first component

--- src/app/two/first/first-routing.module.ts
+++ src/app/two/first/first-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlphaComponent } from './alpha/alpha.component';
+import { BetaComponent } from './beta/beta.component';
+import { FirstComponent } from './first.component';
+import { GammaComponent } from './gamma/gamma.component';
+import { Page404firstComponent } from './page404first/page404first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: FirstComponent, children: [
+      
+      {
+        path: 'alpha', component: AlphaComponent
+      },
+      {
+        path: 'beta', component: BetaComponent
+      },
+      {
+        path: 'gamma', component: GammaComponent
+      },
+      {
+        path: '', redirectTo: 'alpha', pathMatch: 'full'
+      },
+      { path: '**', component:  Page404firstComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
