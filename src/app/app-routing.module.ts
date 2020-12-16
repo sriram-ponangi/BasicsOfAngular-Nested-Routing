609fc0d4a5ded3f72fe96731c6875585c22c2063
Sriram Ponangi <sr810178@dal.ca>
2020-12-16 17:57:17 -0400

    adding routing code and html for app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OneComponent } from './one/one.component';
+import { Page404Component } from './page404/page404.component';
+import { ThreeComponent } from './three/three.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'one', component: OneComponent },
+  { path: 'three', component: ThreeComponent },
+  { path: 'two', loadChildren: () => import(`./two/two.module`).then(m => m.TwoModule) },
+  { path: '', redirectTo: 'one', pathMatch: 'full' },
+  { path: '**', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
