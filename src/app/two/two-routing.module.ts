cba47802f84bcfd11a19eddfe19a47ecd0847023
Sriram Ponangi <sr810178@dal.ca>
2020-12-16 18:20:37 -0400

    adding routing code and html for two component

--- src/app/two/two-routing.module.ts
+++ src/app/two/two-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page404twoComponent } from './page404two/page404two.component';
+import { SecondComponent } from './second/second.component';
+import { TwoComponent } from './two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: TwoComponent, children: [
+      { path: 'second', component: SecondComponent },
+      { path: 'first', loadChildren: () => import(`./first/first.module`).then(m => m.FirstModule) },
+      { path: '', redirectTo: 'second', pathMatch: 'full' },
+      { path: '**', component: Page404twoComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
